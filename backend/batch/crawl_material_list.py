bc7c39df6f512a5261dd7a0b2d815c7a9982e163
pondelion <programming.deve@gmail.com>
2019-09-25 22:19:04 +0900

    update

--- backend/batch/crawl_material_list.py
+++ backend/batch/crawl_material_list.py
@@ -1,15 +1,15 @@
 import sys
 sys.path.append('..')
 import time
-from mi_explorer.crawler.material_list_crawler import MaterialListCrawler
+from threading import Lock
+from mi_explorer.crawler import MaterialListCrawler
 from mi_explorer.data_store.s3 import S3Helper
 from mi_explorer.utils.logger import Logger
 
 
 def main():
-    mlc = MaterialListCrawler(max_id=40000)
-    global finished
-    finished = False
+    mlc = MaterialListCrawler(max_id=4)
+    lock = Lock()
 
     def finish_callback(data):
         try:
@@ -18,27 +18,25 @@ def main():
             s3_helper.save(
                 df=data,
                 s3_path=s3_helper.to_s3path(
-                    f'material_list/{time.strftime("%Y%m%d_%H%M%S")}.csv'
+                    f'materials_project/material_list/{time.strftime("%Y%m%d_%H%M%S")}.csv'
                 )
             )
         finally:
-            global finished
-            finished = True
+            lock.release()
 
     def fail_callback(e):
         Logger.e('material_crawl', e)
-        global finished
-        finished = True
+        lock.release()
 
+    lock.acquire()
     mlc.crawl(
         async_=True,
         on_finish_callback=finish_callback,
         on_fail_callback=fail_callback,
     )
 
-    while finished is False:
-        time.sleep(1)
-    Logger.i('material_crawl', 'finished')
+    with lock:
+        Logger.i('material_crawl', 'finished')
 
 
 if __name__ == '__main__':
---
+++ backend/mi_explorer/crawler/__init__.py
@@ -0,0 +1,1 @@
+from .material_list_crawler import MaterialListCrawler
